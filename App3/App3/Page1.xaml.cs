2b2b43f31f47af971ae237c533d031ca60b9906b
Git-hub-Eduard <81467036+Git-hub-Eduard@users.noreply.github.com>
2021-05-29 18:13:49 +0300

    App3 - универсальное приложение

---
+++ App3/App3/Page1.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Label1.Text = Write.Text;
+        }
+
+        private async void  Button_level_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Game());
+        }
+
+        private async void Settings_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Settings());
+        }
+
+        private void Exit_Clicked(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
